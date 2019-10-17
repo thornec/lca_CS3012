92b0de9ab3f3f3f01d1ef32cdfb423daaf19867b
thornec <thornec@tcd.ie>
2019-10-17 12:08:40 +0100

    Updated DAG class with Simplier Implementation

--- lca/src/lca/dag.java
+++ lca/src/lca/dag.java
@@ -1,119 +1,176 @@
 package lca;
 import java.util.*;
 import java.util.function.Consumer;
+import java.util.function.Consumer;
 
-public class dag<T> {
-
-    private Map<T, Node<T>> nodes;
-    private List<Node<T>> roots;
-
-    public dag() {
-        nodes = new LinkedHashMap<>();
-        roots = new ArrayList<>();
-    }
-
-    /**
-     * Performs Depth-first search from roots and executes lambda on every node
-     *
-     * @param consumer lambda to be executed on nodes
-     */
-    public void visit(Consumer<Node<T>> consumer) {
-        Set<Node<T>> visited = new HashSet<>();
-        for (Node<T> node : roots) {
-            consumer.accept(node);
-            node.getChildren().forEach(n -> n.visit(consumer, visited));
+public class dag {
+	private int V;//Number Vertices
+    private int E;//Number Edges
+    private int[][] adj; //Adjacency list for vertex v - changed to 2D array
+    private int[] outdegree;//Outdegree of Vertex v
+    private int[] indegree; //Indegree of Vertex v
+    private int[] visited;  //Visited Vertices
+    
+    /* Constructor to Initalize an Empty Graph */
+    public dag(int v) {
+    	/* Not enough Verticies */
+        if(V<0){
+            throw new IllegalArgumentException("Number of vertices in the DAG must be greater than 0.");
+        }
+        else{
+            this.V = V;
+            this.E = 0;
+            indegree = new int[V];
+            indegree = new int[V];
+            outdegree = new int[V];
+            visited = new int[V];
+            adj = new int[V][V];
+            for(int i = 0; i<V; i++){//sets up an empty graph in 2D array
+            	for(int j=0;j<V;j++){
+            		adj[i][j] = 0;
+            	}
+            }
         }
     }
 
-    /**
-     * Creates node with given object
-     *
-     * @param object to create node with
-     * @return Node with given object
-     */
-    public Node<T> createNode(T object) {
-        Node<T> node = new Node<>(object);
-        nodes.put(object, node);
-        return node;
+  
+    //Returns no. of Vertices in DAG
+    public int V(){
+        return V;
     }
-
-    /**
-     * Finds root nodes and checks for cycles
-     * @throws CycleFoundException if cycle is found
-     */
-    public void update() throws CycleFoundException {
-        roots.clear();
-        findRoots();
-        checkForCycles();
+    
+    //Returns no. of Edges in DAG
+    public int E(){
+        return E;
     }
-
-    private void findRoots() {
-        for (Node<T> n : nodes.values()) {
-            if (n.getParents().size() == 0)
-                roots.add(n);
+    
+    //throws illegal exception if the vertex put in, is out of bounds
+    private void validateVertex(int v){
+        if((v<0)||(v>=V)){
+            throw new IllegalArgumentException("Edgesless than one means no verices are joined, and if there is a cycle it cannot be moved");
         }
     }
+   
 
-    /**
-     * Checks if graph contains cycles
-     *
-     * @throws CycleFoundException if cycle is found
-     */
-    private void checkForCycles() throws CycleFoundException {
-        if (roots.isEmpty() && nodes.size() > 1)
-            throw new CycleFoundException("No childless node found to be selected as root");
-        List<Node<T>> cycleCrawlerPath = new ArrayList<>();
-        for (Node<T> n : roots) {
-            checkForCycles(n, cycleCrawlerPath);
-        }
+    /* Checks if graph contains cycles */
+    public boolean hasCycle(){
+    	boolean hasCycle=false;
+    	int count = 0;
+    	for(int i =0;i<V;i++){
+    		visited[count]=i;
+    		for(int j = 0; j<V;j++){
+    			for(int k=0;k<V;k++){
+    				if(visited[k]==j && adj[i][j]==1){
+    					hasCycle=true;
+    					return hasCycle;
+    				}
+    			}	
+    		}
+    		count++;
+    	}
+    	return hasCycle;
+    }
+       
+
+   
+
+    /* Adds Directed Edge from V to W */
+    public void addEdge(int v, int w){
+        validateVertex(v);
+        validateVertex(w);
+        adj[v][w]=1;
+        indegree[w]++;
+        outdegree[v]++;
+        E++;
     }
 
-    private void checkForCycles(Node<T> n, List<Node<T>> path) {
-        if (path.contains(n)) {
-            path.add(n);
-            throw new CycleFoundException(getPath(path.subList(path.indexOf(n), path.size())));
-        }
-        path.add(n);
-        n.getParents().forEach(node -> {
-            checkForCycles(node, path);
-        });
-        path.remove(path.size() - 1);
+    /*Removes an edge from v to w */
+    public void removeEdge(int v, int w){
+        validateVertex(v);
+        validateVertex(w);
+        adj[v][w]=0;
+        indegree[w]--;
+        outdegree[v]--;
+        E--;
     }
-
-    /**
-     * @param parent Parent
-     * @param child Child
-     */
-    public void addEdge(T parent, T child) {
-        Node<T> parentNode = getNode(parent);
-        Node<T> childNode = getNode(child);
-        if (parentNode == null) parentNode = createNode(parent);
-        if (childNode == null) childNode = createNode(child);
-        parentNode.addChild(childNode);
+    
+    /*Returns no. of Directed Edges out of Vertex v */
+    public int outdegree(int v){
+    	validateVertex(v);
+    	return outdegree[v];
     }
-
-    public Node<T> getNode(T key) {
-        return nodes.get(key);
+    
+    /*Returns no. of Directed Edges in to Vertex v */
+    public int indegree(int v){
+    	validateVertex(v);
+    	return indegree[v];
     }
-
-    public Collection<Node<T>> getNodes() {
-        return nodes.values();
+    
+    /*Returns the Vertices adjacent from Vertex v */
+    public int[] adj(int v){
+    	validateVertex(v);
+    	int[] temp = new int[outdegree[v]];
+    	int count =0;
+    	for(int i=0;i<V;i++){
+    		if(adj[v][i]==1){
+    			temp[count]=i;
+    			count++;
+    	    }
+    	}
+    	return temp;
     }
-
-    private String getPath(List<Node<T>> path) {
-        StringBuilder sb = new StringBuilder();
-        for (int i = 0; i < path.size() - 1; i++) {
-            sb.append(path.get(i).getObject().toString());
-            sb.append("->");
-        }
-        sb.append(path.get(path.size() - 1).getObject().toString());
-        return sb.toString();
+    
+  //This public function is used to find the lowest common ancestor in a directed acyclic graph
+    public int findLCA(int v, int w){
+	validateVertex(v);
+	validateVertex(w);
+	boolean hasCycle = hasCycle();
+	if(E>0 && !hasCycle){
+		return LCAUtil(v,w);
+	}
+	else{
+		throw new IllegalArgumentException("This graph is not an acyclic graph.");
+	}
     }
-
-    @Override
-    public String toString() {
-        return "DAG{" +
-                "nodes.size=" + nodes.size() +
-                '}';
-    }
+    
+    //helper function for LCA
+    private int LCAUtil(int v, int w){
+	int[] vArr = new int[E];
+	int[] wArr = new int[E];
+	boolean[] vMarked = new boolean[V];
+	boolean[] wMarked = new boolean[V];
+	int vCount =0;
+	int wCount = 0;
+	vArr[vCount]=v;
+	wArr[wCount]=w;
+	for(int j=0; j<V;j++){//mark all vertices as not been visited yet
+		vMarked[j]=false;
+		wMarked[j]=false;
+	}
+	for(int i =0;i<V;i++){
+		vMarked[v] =true;
+		wMarked[w] =true;
+		for(int j = 0; j<V;j++){
+			if(adj[i][j]==1 && vMarked[i]){
+				vCount++;
+				vArr[vCount]=j;
+				vMarked[j]=true;
+			}
+			if(adj[i][j]==1 && wMarked[i]){
+				wCount++;
+				wArr[wCount]=j;
+				wMarked[j]=true;
+			}
+			if(wArr[wCount]==vArr[vCount]){
+				return wArr[wCount];
+			}
+		}
+	}
+	return -1;//returns -1 if no ancestor found
+   }  
+    
+  
+  		
+  		
+    
 }
